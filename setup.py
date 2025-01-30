b03bf55582abc827dacdb33615f1ae95c572fdfe
jbianchi81 <jbianchi@ina.gob.ar>
2025-01-30 17:54:06 -0300

    versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='a5_client',
-    version='0.1.5',
+    version='0.1.6',
     packages=find_packages(),
     description='a5 API client',
     author='Juan F. Bianchi',
