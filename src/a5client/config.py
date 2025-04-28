00d76cb742fce238a5ce7c617dbcc400f224eedd
jbianchi81 <jbianchi@ina.gob.ar>
2025-04-28 16:57:18 -0300

    read default config, write config

--- src/a5client/config.py
+++ src/a5client/config.py
@@ -3,15 +3,29 @@ import os
 
 defaults = {
     "log": {
-        "filename": "/var/log/a5client.txt"
+        "filename": "/var/log/a5client.log"
     }
 }
 
 config_path = os.path.join(os.environ["HOME"],".a5client.ini")
 
+def write_config(file_path : str = config_path, overwrite : bool = False, raise_if_exists : bool = False):
+    config = configparser.ConfigParser()
+    config.add_section("log")
+    config.set("log","filename",defaults["log"]["filename"])
+    if os.path.exists(file_path) and overwrite is False:
+        if raise_if_exists:
+            raise ValueError("Config file already exists")
+    else:
+        config.write(open(file_path,"w"))
+
 def read_config(file_path : str = config_path) -> configparser.ConfigParser:
     config = configparser.ConfigParser()
-    config.read(file_path)
+    if os.path.exists(file_path):
+        config.read(file_path)
+    else:
+        config.add_section("log")
+        config.set("log","filename",defaults["log"]["filename"])
 
     # # Access sections and options
     # for section in config.sections():
