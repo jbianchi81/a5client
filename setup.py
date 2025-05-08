091f5d7944aa2a78fece506572cb3792d393e98a
jbianchi81 <jbianchi@ina.gob.ar>
2025-05-08 18:49:16 -0300

    vrsn

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='a5_client',
-    version='0.1.10.2',
+    version='0.1.10.3',
     packages=find_packages(),
     description='a5 API client',
     author='Juan F. Bianchi',
